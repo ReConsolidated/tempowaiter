b1d1eda8d27213906ebe32213522c845914af3d5
ReConsolidated <gracjanpasik@gmail.com>
2024-01-05 00:43:11 +0100

    0.3.0 migrate performance data controller

--- src/main/java/io/github/reconsolidated/tempowaiter/waitingCompanyAssignment/WaitingCompanyAssignment.java
+++ src/main/java/io/github/reconsolidated/tempowaiter/waitingCompanyAssignment/WaitingCompanyAssignment.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 @AllArgsConstructor
@@ -14,6 +16,8 @@ import javax.persistence.Entity;
 @Getter
 @Setter
 public class WaitingCompanyAssignment {
+    @Id
+    @GeneratedValue
     private Long id;
     private Long companyId;
     @Column(unique = true)
