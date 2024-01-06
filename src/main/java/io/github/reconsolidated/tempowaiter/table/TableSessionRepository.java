dc0d5f7658c68e5e4ef677b6aa165c2d30ed022d
ReConsolidated <gracjanpasik@gmail.com>
2024-01-06 14:40:30 +0100

    1.0.0 jwt auth

--- src/main/java/io/github/reconsolidated/tempowaiter/table/TableSessionRepository.java
+++ src/main/java/io/github/reconsolidated/tempowaiter/table/TableSessionRepository.java
@@ -1,16 +1,16 @@
 package io.github.reconsolidated.tempowaiter.table;
 
-import com.sun.xml.bind.v2.schemagen.episode.SchemaBindings;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface TableSessionRepository extends JpaRepository<TableSession, Long> {
 
     Optional<TableSession> findBySessionIdAndCardIdAndExpirationDateGreaterThanAndIsOverwrittenFalse(String sessionId, Long cardId, LocalDateTime now);
 
-    Optional<TableSession> findByCardIdAndIsOverwrittenFalseAndExpirationDateGreaterThan(Long cardId, LocalDateTime expirationDate);
+    List<TableSession> findAllByCardIdAndIsOverwrittenFalseAndExpirationDateGreaterThan(Long cardId, LocalDateTime expirationDate);
 
     Optional<TableSession> findBySessionIdAndExpirationDateGreaterThanAndIsOverwrittenFalse(String sessionId, LocalDateTime now);
 }
